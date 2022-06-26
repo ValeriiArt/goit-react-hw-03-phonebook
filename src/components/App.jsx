fe5c6e31f03661a1a5188c479b899122a58115fb
Valerii Artemenko <87828092+ValeriiArt@users.noreply.github.com>
2022-06-26 16:58:18 +0300

    add function save on localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -51,6 +51,20 @@ export class App extends Component {
     // console.log(id);
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsetContacts = JSON.parse(contacts);
+    if (parsetContacts) {
+      this.setState({ contacts: parsetContacts})
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const { filter } = this.state;
     return (
